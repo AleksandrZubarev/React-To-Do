f707ea1abf740b9672e116d03d86c493d83464c2
Aleksandr <sir.zubareff2011@yandex.ru>
2022-01-30 00:00:12 +0300

    Props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,19 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: "John", salary: 800, increase: true},
+        {name: "Alex", salary: 3000},
+        {name: "Carl", salary: 5000},
+        {name: "Den", salary: 5000},
+        {name: "Ron", salary: 7000},
+        {name: "Sly", salary: 9000},
+        {name: "Rut", salary: 8000},
+        {name: "Sendi", salary: 8000},
+        {name: "Серёга", salary: 1},
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -16,7 +29,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployersList/>
+            <EmployersList data={data}/>
 
             <EmployersAddForm/>
             
